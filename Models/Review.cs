0829a95e9da5d2836b0abce99e93575311fbed43
Aayush Jain <jain.aay@northeastern.edu>
2020-08-05 15:52:59 -0400

    Acknowledgement page and Review Model

---
+++ Models/Review.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DayCare.Models
+{
+    public class Review
+    {
+        public string reviewID { get; set; }
+        public string studentID {get; set;}
+        public string studentName { get; set; }
+        public string teacherID { get; set; }
+        public string teacherName { get; set; }
+        public DateTime date_of_review { get; set; }
+        public string feedback { get; set; }
+        public Review()
+        {
+            date_of_review = DateTime.Now;
+        }
+
+
+    }
+}
